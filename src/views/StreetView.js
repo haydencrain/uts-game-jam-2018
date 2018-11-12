bb4b1cde36ab421b11d91a604d556ab73fa9bc82
Hayden Crain <haydencrain@optusnet.com.au>
2018-11-13 10:49:09 +1100

    rename to street view

--- src/views/StreetView.js
+++ src/views/StreetView.js
@@ -1,31 +1,44 @@
 import Grandma from '../sprites/Grandma';
+import { getRandomX } from '../utils';
+import RunningCorgi from '../sprites/RunningCorgi';
 
-export default class GrandmaView {
+
+export default class StreetView {
   constructor({ gameState, height, width, x, y }) {
     this.gameState = gameState;
     this.height = height;
     this.width = width;
     this.x = x;
     this.y = y;
+    this.grandmas = [];
+
+    this.floorY = height - 30;
 
     this.create = this.create.bind(this);
     this.update = this.update.bind(this);
     this.getCenterX = this.getCenterX.bind(this);
     this.getCenterY = this.getCenterY.bind(this);
+    this.createGrandma = this.createGrandma.bind(this);
   }
 
   create() {
     // add background
     this.background = this.gameState.add.tileSprite(this.x, this.y, this.width, this.height, 'street_background');
 
-    this.grandma = new Grandma({
+    this.grandma = this.createGrandma();
+
+    // delete later
+    this.runningDog = new RunningCorgi({
       game: this.gameState.game,
       x: this.getCenterX(),
-      y: this.getCenterY() + 10,
+      y: this.getCenterY(),
       scaleX: 2,
       scaleY: 2,
       anchor: 0.5,
+      inputEnabled: true,
     });
+    this.runningDog.run();
+    this.runningDog.addOnClickListener(() => this.grandmas.push(this.createGrandma()));
   }
 
   update() {
@@ -38,5 +51,16 @@ export default class GrandmaView {
 
   getCenterY() {
     return (this.height / 2) + this.y;
+  }
+
+  createGrandma() {
+    return new Grandma({
+      game: this.gameState.game,
+      x: getRandomX(this.x + 150, 300),
+      y: this.floorY,
+      scaleX: 2,
+      scaleY: 2,
+      anchor: 1,
+    });
   }
 }
