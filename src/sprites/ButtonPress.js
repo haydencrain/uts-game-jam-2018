16ac161d3bb72b1233cf935efadbecd6297c3bec
Sheng <shengzengg@gmail.com>
2018-11-13 04:16:37 +1100

    buttons now increment the multiplier

--- src/sprites/ButtonPress.js
+++ src/sprites/ButtonPress.js
@@ -1,15 +1,34 @@
 import Phaser from 'phaser';
+import config from '../config';
+
 
 export default class extends Phaser.Sprite {
-  constructor({ game, x, y, scaleX, scaleY, anchor, asset, inputEnabled = false }) {
-    super(game, x, y, asset);
+  constructor({ gameState, x, y, scaleX, scaleY, anchor, asset, inputEnabled = false, price, increment, message }) {
+    super(gameState.game, x, y, asset);
     this.scale.setTo(scaleX, scaleY);
     this.anchor.setTo(anchor); // set middle
-    game.add.existing(this);
+    this.game.add.existing(this);
+    this.gameState = gameState;
+    this.x = x;
+    this.y = y;
+    this.isActive = false;
+    this.isHeld = false;
+
+    this.increment = increment;
+    this.message = message;
 
     this.press = this.press.bind(this);
-
+    this.purchase = this.purchase.bind(this);
+    this.price = price;
     this.inputEnabled = inputEnabled;
+
+    this.text = this.gameState.add.text(this.x + 50, this.y + 30, this.message, {
+      font: config.submenuFont,
+      fill: '#77BFA3',
+      smoothed: false,
+      align: 'center',
+    });
+
     if (this.inputEnabled) {
       this.events.onInputOver.add(() => {
         this.game.canvas.style.cursor = 'pointer';
@@ -24,12 +43,30 @@ export default class extends Phaser.Sprite {
     this.animations.add('inactive', [2], false);
   }
 
+  create() {
+
+  }
+
   unpress() {
-    this.play('active');
+    if (this.isActive) {
+      this.text.x -= 2;
+      this.text.y -= 2;
+    }
+    this.isHeld = false;
+    this.isActive = true;
   }
 
   press() {
-    this.play('pressed');
+    if (this.isActive) {
+      this.isHeld = true;
+      this.text.x += 2;
+      this.text.y += 2;
+    }
+  }
+
+  inactive() {
+    this.isActive = false;
+    this.isHeld = false;
   }
 
 
@@ -44,5 +81,22 @@ export default class extends Phaser.Sprite {
   }
 
   update() {
+    this.gameState.globalData.score < this.price ? this.isActive = false : this.isActive = true;
+
+
+    if (this.isActive && !this.isHeld) {
+      this.play('active');
+    } else if (this.isHeld) {
+      this.play('pressed');
+    } else {
+      this.play('inactive');
+    }
+  }
+
+  purchase() {
+    if (this.isActive) {
+      this.gameState.globalData.multiplier += this.increment;
+      this.gameState.globalData.score -= this.price;
+    }
   }
 }
