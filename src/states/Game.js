0acd816727e881eaba0106f924a983a45ea7de6a
Hayden Crain <haydencrain@optusnet.com.au>
2018-11-13 15:47:14 +1100

    remove hardcoded score

--- src/states/Game.js
+++ src/states/Game.js
@@ -6,7 +6,7 @@ import MenuView from '../views/MenuView';
 export default class extends Phaser.State {
   init() {
     this.globalData = {
-      score: 100000000000,
+      score: 0,
       multiplier: 1,
     };
 
